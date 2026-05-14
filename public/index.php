0f2a0c61a950fc70890baf18aa94e5e1f5da51c5
AcE <acecumbe4@gmail.com>
2026-05-15 01:30:21 +0800

    done adding use Framework\Database; and done watching lab 3

--- public/index.php
+++ public/index.php
@@ -3,9 +3,19 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+
+use Framework\Router;
+
+
+// require '../helpers.php';
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+// $config = require basePath('config/db.php');
+
+// $db = new Database($config);
 
 // Instatiate the router
 $router = new Router();
