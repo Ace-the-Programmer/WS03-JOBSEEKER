2141af7f611175952a8cb45982a9307fd1c030ba
AcE <acecumbe4@gmail.com>
2026-05-16 23:25:49 +0800

    done Crud part 3: created the edit job and added validation to check for missing fields when updating, and built reusable success/error messages.

--- routes.php
+++ routes.php
@@ -1,8 +1,16 @@
 <?php
-
+//get
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
+
+//post
 $router->post('/listings', 'ListingController@store');
+
+//put
+$router->put('/listings/{id}', 'ListingController@update');
+
+//delete
 $router->delete('/listings/{id}', 'ListingController@destroy');
