345e5332cedde61be0d996bb1237d5332a4dab7f
AcE <acecumbe4@gmail.com>
2026-05-17 16:52:38 +0800

    Implement user login authentication

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -14,12 +14,12 @@ use Framework\Session;
                     <form method="POST" action="/auth/logout">
                         <button type="submit" class="text-white inline hover:underline">Logout</button>
                     </form>
+                    <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
                 </div>
             <?php else : ?>
                 <a href="/auth/login" class="text-white hover:underline">Login</a>
                 <a href="/auth/register" class="text-white hover:underline">Register</a>
             <?php endif; ?>
-            <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
         </nav>
     </div>
 </header>
