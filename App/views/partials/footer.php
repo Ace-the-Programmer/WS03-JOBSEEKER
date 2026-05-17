89cc38c011dfdb05c145fd10848e4b5db43a752d
AcE <acecumbe4@gmail.com>
2026-05-17 22:34:02 +0800

    adjusting the UI

--- App/views/partials/footer.php
+++ App/views/partials/footer.php
@@ -1,5 +1,6 @@
-<footer class="bg-blue-900 text-white py-8 mt-auto">
-    <div class="container mx-auto text-center">
+<footer class="container mx-auto bg-blue-900 text-white py-6 mt-auto">
+    <hr class="mb-4 mx-4 border-gray-400">
+    <div class="text-center">
         <p>&copy; 2026 Jobseek. All rights reserved.</p>
     </div>
 </footer>
